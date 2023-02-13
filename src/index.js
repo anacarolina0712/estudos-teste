3ac6d752d80f2d112c5e40e40422a47a05f510b5
ana carolina <aalvesdacostamoura@gmail.com>
2023-02-13 08:17:47 -0300

    teste da calculadora

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css';
+import Calculator  from './main/Calculator';
+import  reportWebVitals from './reportWebVitals';
+
+ReactDom.render(
+  <div className='Calculadora'>
+    <h1>Calculadora</h1>
+    <Calculator/>
+    
+  </div>
+  , document.getElementById('root')
+);
+reportWebVitals();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import Calculator from './main/Calculator';
+// import reportWebVitals from './reportWebVitals';
+
+// reportWebVitals();
+// ReactDOM.render(
+//   <div>
+//   <h1>Calculadora</h1>
+//   <Calculator/>
+//   </div>
+//   ,document.getElementById('root'));
+
+
